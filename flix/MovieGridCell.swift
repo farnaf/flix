f9e1a6b4a75f5681b3fe1cd4517384b0212b982f
farnaf <fnafarif@ucsd.edu>
2021-04-14 22:18:29 -0700

    assignment 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Farnia Nafarifard on 4/14/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
